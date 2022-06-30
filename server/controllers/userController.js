059346313ea08a1a06ef4bb284e1af821b295381
AlexITHoot <alex.pylypchuk@ithoot.com>
2022-06-30 19:11:38 +0300

    Login,password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,50 @@
+const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const {User,Basket} = require('../models/models')
+const jwt = require('jsonwebtoken')
+const generateJwt = (id,email,role)=>{
+         return jwt.sign(
+            {id,email,role},
+            process.env.SECRET_KEY,
+            {expiresIn:'24h'}
+         )
+}
+
 class UserController{
-   async registration(req,res){
-
+   async registration(req,res,next){
+      const {email,password,role} = req.body
+      if(!email || !password){
+         return next(ApiError.badRequest('Not correct email or password'))
+      }
+      const candidate = await User.findOne({where:{email}})
+      if(candidate){
+         return next(ApiError.badRequest('User with this email address exists'))
+      }
+      const hashPassword = await bcrypt.hash(password,5)
+      const user = await User.create({email,role,password:hashPassword})
+      const basket = await Basket.create({userId:user.id})
+      const token = generateJwt(user.id,user.email,user.role)
+      return res.json({token})
    }
-   async login(req,res){
-
+   async login(req,res,next){
+      const {email,password} = req.body
+      const user = await User.findOne({where:{email}})
+      if(!user){
+         return next(ApiError.badRequest('User not founded'))
+      }
+      let comparePassword = bcrypt.compareSync(password,user.password)
+      if(!comparePassword){
+         return next(ApiError.badRequest('Bad password'))
+      }
+      const token = generateJwt(user.id,user.email,user.role)
+      return res.json({token})
    }   
-   async check(req,res){
-      const query = req.query
-      res.json(query)
+   async check(req,res,next){
+      const {id} = req.query
+      if(!id){
+         return next(ApiError.badRequest('No ID'))
+      }
+      res.json(id)
    }      
 }
 
--- server/routs/userRouter.js
+++ server/routs/userRouter.js
@@ -1,12 +1,11 @@
 const Router = require('express')
-const userController = require('../controllers/userController')
 const router = new Router()
 const UserController = require('../controllers/userController')
 
 
-router.post('/registration',userController.registration)
-router.post('/login',userController.login)
-router.get('/auth',userController.check)
+router.post('/registration',UserController.registration)
+router.post('/login',UserController.login)
+router.get('/auth',UserController.check)
 
 
 module.exports = router;
